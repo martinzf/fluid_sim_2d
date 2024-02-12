78aae34531a4a71e1d1e02e7f85da9d115a34903
Martin Zapata <martzapa@ucm.es>
2024-02-12 14:02:17 +0100

    animation blitting

--- animated_plot.py
+++ animated_plot.py
@@ -9,9 +9,6 @@ plt.style.use('fast')
 FPS = 40
 
 def init(x, y, vmin, vmax):
-    plt.title('Vorticity simulation')
-    plt.xlabel('x (m)')
-    plt.ylabel('y (m)')
     plt.xlim(x[0, 0], x[0, -1])
     plt.ylim(y[0, 0], y[-1, 0])
     z = np.empty(np.shape(x))
@@ -20,8 +17,9 @@ def init(x, y, vmin, vmax):
     lims = plt.contourf(x, y, z)
     cbar = plt.colorbar(lims)
     cbar.set_label('$\omega$ (s$^{-1}$)')
-    for child in lims.collections:
-        child.remove()
+    for c in lims.collections:
+        c.remove()
+    lims = []
 
 def downsampling(w, dt):
     if dt < 1 / FPS:
@@ -32,16 +30,19 @@ def downsampling(w, dt):
     return w, 1e3 / FPS
 
 fig = plt.figure('Simulation', figsize=(7, 6))
+plt.title('Vorticity simulation')
+plt.xlabel('x (m)')
+plt.ylabel('y (m)')
 cont = plt.contourf(np.empty((2, 2)), np.empty((2, 2)), np.empty((2, 2)))
 def animate(x, y, w, dt):
     w_d, interval = downsampling(w, dt)
     vmin, vmax = np.min(w_d), np.max(w_d)
-    init_func = lambda : init(x, y, vmin, vmax)
+    init(x, y, vmin, vmax)
     def func(i):
         global cont
-        for child in cont.collections:
-            child.remove()
+        for c in cont.collections:
+            c.remove()
         cont = plt.contourf(x, y, w_d[i], vmin=vmin, vmax=vmax)
-    ani = animation.FuncAnimation(fig, func, init_func=init_func, 
-                                  frames=w_d.shape[0], interval=interval, repeat=False)
+        return cont.collections
+    ani = animation.FuncAnimation(fig, func, frames=w_d.shape[0], interval=interval, blit=True, repeat=False)
     plt.show()
